274f04f1f9db43126f6aa7aaa160c5b6e283dd4c
Liliana Lessa <llessa.smo@eos-uptrade.de>
2024-09-27 11:00:37 +0100

    app - decoupling sub app div ids from  container

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productMount} from 'products/ProductsIndex';
+import { mount as cartMount} from 'cart/CartShow';
 
 console.log('Bootstrap.js loaded');
+
+productMount(document.querySelector('#products-container'));
+cartMount(document.querySelector('#cart-container'));
